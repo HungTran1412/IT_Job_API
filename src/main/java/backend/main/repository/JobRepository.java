d1efc8ccf642503290b8ffbb6fbad4c0d08e41f7
wira <95398622+wadiobao@users.noreply.github.com>
2025-11-05 09:16:54 +0700

    Job attributes

---
+++ src/main/java/backend/main/repository/JobRepository.java
@@ -0,0 +1,10 @@
+package backend.main.repository;
+
+import backend.main.entities.Admin;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface JobRepository extends CrudRepository<Job,String> {
+    Optional<Job> findByTilte(String tilte);
+}
