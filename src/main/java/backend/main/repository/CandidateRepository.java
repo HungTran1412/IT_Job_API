2477c52e41a58858f37183226b0f831da4a12a24
Hung Tran Nguyen Minh <hug2k4123@gmail.com>
2026-01-02 16:21:07 +0700

    feat(api): thêm api lấy tất cả người dùng cho admin

--- src/main/java/backend/main/repository/CandidateRepository.java
+++ src/main/java/backend/main/repository/CandidateRepository.java
@@ -1,11 +1,11 @@
 package backend.main.repository;
 
 import backend.main.entities.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface CandidateRepository extends CrudRepository<Candidate, String>, JpaSpecificationExecutor<Candidate> {
+public interface CandidateRepository extends JpaRepository<Candidate, String>, JpaSpecificationExecutor<Candidate> {
     Optional<Candidate> findByEmail(String email);
 }
--- src/main/java/backend/main/services/AdminService.java
+++ src/main/java/backend/main/services/AdminService.java
@@ -3,10 +3,14 @@ package backend.main.services;
 import backend.main.dto.request.ChangePasswordRequest;
 import backend.main.dto.request.LoginRequest;
 import backend.main.dto.request.admin.AdminRegisterRequest;
+import backend.main.dto.response.UserSummaryResponse;
 import backend.main.entities.Admin;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface AdminService {
     String login(LoginRequest request);
     boolean changePassword(String email, ChangePasswordRequest request);
     void updateUserLockStatus(String userId, boolean isLocked);
+    Page<UserSummaryResponse> getAllUsers(Pageable pageable, String role);
 }
